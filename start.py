1b71b1a88a0f948e450762d2a5fc6e2630e6b02b
posthello-code <mail@alexjensen.net>
2024-05-01 16:58:04 -0700

    add initial flask server implementation

--- start.py
+++ start.py
@@ -1,7 +1,44 @@
 from flask import Flask
+from flask_restful import Api
+from services.postgres import init_postgres, sources, stories
+from flask_restful import fields, marshal_with
 
-# todo add some controllers?
+
+session = init_postgres()
 app = Flask(__name__)
-    
-if __name__ == '__main__':
-    app.run()
+api = Api(app)
+
+
+@app.route("/stories")
+@marshal_with(
+    {
+        "id": fields.String,
+        "createdDate": fields.String,
+        "sourceId": fields.String,
+        "title": fields.String,
+        "summary": fields.String,
+    }
+)
+def get_stories():
+    items = session.query(stories).all()
+    return items
+
+
+@app.route("/sources")
+@marshal_with(
+    {
+        "id": fields.String,
+        "source": fields.String,
+        "createdDate": fields.String,
+        "sourceMethod": fields.String,
+        "sourceUri": fields.String,
+        "content": fields.String,
+    }
+)
+def get_sources():
+    items = session.query(sources).order_by("createdDate").all()
+    return items
+
+
+if __name__ == "__main__":
+    app.run(debug=False)

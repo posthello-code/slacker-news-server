c8fdcbd69395043adaa38b8a761ace3fe9a36c53
posthello-code <github@alexjensen.net>
2024-05-22 22:31:25 -0700

    alembic migrations, and add externalId to sources

---
+++ alembic/versions/bdbcab3ca88b_add_externalid.py
@@ -0,0 +1,27 @@
+"""add externalId
+
+Revision ID: bdbcab3ca88b
+Revises: 
+Create Date: 2024-05-22 21:40:16.052245
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "bdbcab3ca88b"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("sources", sa.Column("externalId", sa.Integer))
+
+
+def downgrade() -> None:
+    op.drop_column("sources", "externalId")

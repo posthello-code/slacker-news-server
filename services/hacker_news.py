e31c0291a4447021c2639627265027a1b085441f
posthello-code <github@alexjensen.net>
2024-05-23 10:33:24 -0700

    externalId to sources

--- services/hacker_news.py
+++ services/hacker_news.py
@@ -12,7 +12,7 @@ def request_top_story():
     topStories = eval(response.text)
     response = requests.get(
         "https://hacker-news.firebaseio.com/v0/item/% s.json?print=pretty"
-        % topStories[0]
+        % topStories[3]
     )
 
     return response
@@ -23,13 +23,13 @@ def sqlTemplateFromTopStory(
 ):
     topStoryResponse = request_top_story()
     topStoryContent = json.dumps(topStoryResponse.text)
+    topStoryId = json.loads(topStoryResponse.text)["id"]
 
     try:
         # handle when the story is an external website
         topStoryUri = json.loads(topStoryResponse.text)["url"]
     except:
         # handle when the story internal to hacker news
-        topStoryId = json.loads(topStoryResponse.text)["id"]
         topStoryUri = "https://news.ycombinator.com/item?id=" + str(topStoryId)
 
     print(topStoryUri)
@@ -42,6 +42,7 @@ def sqlTemplateFromTopStory(
         sourceUri=topStoryResponse.url,
         dataFormat="json",
         content=topStoryContent,
+        externalId=topStoryId,
     )
 
     # save to postgres
@@ -58,6 +59,7 @@ def sqlTemplateFromTopStory(
         sourceUri=topStoryUri,
         dataFormat="html",
         content=topStorySiteHtml.text,
+        externalId=topStoryId,
     )
 
     # save to postgres

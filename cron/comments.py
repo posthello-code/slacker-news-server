ff80799c7a8ffe8c088fce2a5ab6c759195aad47
posthello-code <github@alexjensen.net>
2024-05-31 01:00:14 -0700

    save comment summary to database

--- cron/comments.py
+++ cron/comments.py
@@ -1,29 +1,29 @@
 import json
+
+from sqlalchemy import desc
 from services.postgres import init_postgres, close_session
 from models.data_models import Source, Comment
 from services.openai import doCompletionWithSystemMessage, optimizeTextForCompletion
 
 session = init_postgres()
 
-print("summarize the comments")
-
 comment_sources = (
     session.query(Source)
-    .order_by("createdDate")
-    .where(Source.source == "hacker-news-comments")
+    .order_by(desc("createdDate"))
+    .where(Source.source == "hacker-news-comment")
     .all()
 )
 
 latest_comments_obj = comment_sources[0]
 latest_comments = json.dumps(latest_comments_obj.content)
-
-print(latest_comments)
+# print(latest_comments)
 text = optimizeTextForCompletion(latest_comments)
 summary = doCompletionWithSystemMessage(
     text,
     """You are being provided comment data for a hacker news article, summarize general sentiment. 
     Provide a few interesting quotes.
-    Limit the length to 250 characters""",
+    Limit the length to 250 characters.
+    Don't repeat yourself""",
 )
 
 print(summary)
@@ -32,7 +32,13 @@ existing_comments = session.query(Comment).where(
     Comment.sourceId == latest_comments_obj.id
 )
 if len(existing_comments.all()) == 0:
-    session.add(Comment(summary=summary, sourceId=latest_comments_obj.id))
+    session.add(
+        Comment(
+            summary=summary,
+            sourceId=latest_comments_obj.id,
+            externalId=latest_comments_obj.externalId,
+        )
+    )
     session.commit()
 else:
     print("update existing")
